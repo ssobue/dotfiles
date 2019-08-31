83da05428a4cffdf48b4b11db7377a346ff14e43
Sho Sobue <meru26.sho@gmail.com>
2019-08-31 23:53:23 +0900

    remove bash

--- bin/setup-dotfiles.sh
+++ bin/setup-dotfiles.sh
@@ -7,7 +7,7 @@ function link_file() {
   ORIG_FILE=$1
   TARGET_FILE=$2
   # backup exist file
-  if [ -f "${TARGET_FILE}" -o -h "${TARGET_FILE}" ]; then
+  if [[ -f "${TARGET_FILE}" || -h "${TARGET_FILE}" ]]; then
     mv -fv "${TARGET_FILE}" "${TARGET_FILE}.bak"
   fi
 
@@ -15,27 +15,20 @@ function link_file() {
 }
 
 function setup_clone_dir() {
-  if [ ! -d "${CHECKOUT_DIR}" ]; then
+  if [[ ! -d "${CHECKOUT_DIR}" ]]; then
     mkdir "${CHECKOUT_DIR}"
   fi
 
   cd "${CHECKOUT_DIR}"
 
   for REPO in ${CHECKOUT_REPOSITORY}; do
-    if [ ! -d "${REPO}" ]; then
+    if [[ ! -d "${REPO}" ]]; then
       git clone https://github.com/ssobue/${REPO}.git
     fi
   done
 }
 
 function setup_mac_setting() {
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bash_profile ~/.bash_profile
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bashrc.mac ~/.bashrc
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bash_env.mac ~/.bash_env
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bash_alias.bsd ~/.bash_alias
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bash_color ~/.bash_color
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bash_function ~/.bash_function
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bash_sharehistory ~/.bash_sharehistory
   link_file ${CHECKOUT_DIR}/dotfiles/etc/zshenv.mac ~/.zshenv
   link_file ${CHECKOUT_DIR}/dotfiles/etc/zshrc.mac ~/.zshrc
   link_file ${CHECKOUT_DIR}/dotfiles/etc/gitconfig ~/.gitconfig
@@ -43,27 +36,22 @@ function setup_mac_setting() {
 }
 
 function setup_linux_setting() {
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bash_profile ~/.bash_profile
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bashrc.linux ~/.bashrc
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bash_env.linux ~/.bash_env
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bash_alias.gnu ~/.bash_alias
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bash_color ~/.bash_color
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bash_function ~/.bash_function
-  link_file ${CHECKOUT_DIR}/dotfiles/etc/bash_sharehistory ~/.bash_sharehistory
+  link_file ${CHECKOUT_DIR}/dotfiles/etc/zshenv.linux ~/.zshenv
+  link_file ${CHECKOUT_DIR}/dotfiles/etc/zshrc.linux ~/.zshrc
   link_file ${CHECKOUT_DIR}/dotfiles/etc/gitconfig ~/.gitconfig
   link_file ${CHECKOUT_DIR}/dotfiles/etc/vimrc ~/.vimrc
 }
 
 # check git command
-if [ ! -x /usr/bin/git -a ! -x /usr/local/bin/git ]; then
+if [[ ! -x /usr/bin/git && ! -x /usr/local/bin/git ]]; then
   exit 1
 fi
 
 # choose architecture
-if [ "$(uname)" = "Linux" ]; then
+if [[ "$(uname)" = "Linux" ]]; then
   setup_clone_dir
   setup_linux_setting
-elif [ "$(uname)" = "Darwin" ]; then
+elif [[ "$(uname)" = "Darwin" ]]; then
   setup_clone_dir
   setup_mac_setting
 else
